d776285a609a1b736aca1d4d57549c6885956641
Leonard Steppy <leonard.steppy@outlook.de>
2025-10-22 10:28:50 +0200

    Declare five as constant

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::num::NonZeroU32;
 
+const FIVE: NonZeroU32 = NonZeroU32::new(5).expect("not zero");
+
 #[inline]
 pub fn double(cents: u32) -> u32 {
   cents * 2
@@ -10,26 +12,28 @@ pub fn round_down(cents: u32, to: NonZeroU32) -> u32 {
   cents / to * to.get()
 }
 
+#[inline]
+pub fn round_down_to_five(cents: u32) -> u32 {
+  round_down(cents, FIVE)
+}
+
 #[cfg(test)]
 mod test {
-  use crate::{double, round_down};
-  use std::num::NonZeroU32;
+  use crate::{double, round_down_to_five};
 
   #[test]
   fn test_round_down() {
-    let round_to = NonZeroU32::new(5).unwrap();
-    assert_eq!(5, round_down(7, round_to));
-    assert_eq!(0, round_down(3, round_to));
-    assert_eq!(195, round_down(198, round_to));
+    assert_eq!(5, round_down_to_five(7));
+    assert_eq!(0, round_down_to_five(3));
+    assert_eq!(195, round_down_to_five(198));
   }
 
   #[test]
   fn test_double_and_round() {
-    let round_to = NonZeroU32::new(5).unwrap();
-    assert_eq!(200, round_down(double(100), round_to));
-    assert_eq!(205, round_down(double(103), round_to));
-    assert_eq!(210, round_down(double(105), round_to));
-    assert_eq!(50, round_down(double(25), round_to));
-    assert_eq!(215, round_down(double(108), round_to));
+    assert_eq!(200, round_down_to_five(double(100)));
+    assert_eq!(205, round_down_to_five(double(103)));
+    assert_eq!(210, round_down_to_five(double(105)));
+    assert_eq!(50, round_down_to_five(double(25)));
+    assert_eq!(215, round_down_to_five(double(108)));
   }
 }
