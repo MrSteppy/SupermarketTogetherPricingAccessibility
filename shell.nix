7114b3008b85243c6522f30feeda2b45309ec091
Leonard Steppy <leonard.steppy@outlook.de>
2025-11-03 11:40:30 +0100

    Figure out how to use rdev to listen to events

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
+    pkg-config
+    xorg.libX11
+    xorg.libXi
   ];
   buildInputs = with pkgs; [
   ];
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use std::thread::sleep;
 use std::time::Duration;
+use rdev::{EventType, Key};
 use supermarket_together_pricing_accessibility::keyboard::Keyboard;
 
 fn main() {
+  run();
+
   sleep(Duration::from_secs(3));
   let mut keyboard = Keyboard::new().expect("keyboard");
   keyboard.type_on_numpad().expect("type on numpad");
+}
+
+fn run() {
+  rdev::listen(|event| {
+    if let EventType::KeyPress(key) = event.event_type {
+      match key {
+        Key::Num1 => {
+          println!("User typed 1!");
+        }
+        _ => {}
+      }
+    }
+  }).expect("failed to key listener");
 }
