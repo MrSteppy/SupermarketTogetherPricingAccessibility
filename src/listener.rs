67a28f26eb5fd175a0cf2e15e351c9bc8076c84a
Leonard Steppy <leonard.steppy@outlook.de>
2025-11-04 17:31:28 +0100

    Replace rdev by device_query

--- src/listener.rs
+++ src/listener.rs
@@ -1,47 +1,38 @@
-use crate::input::Input;
+use crate::input::{Character, Digit, Input};
+use device_query::{DeviceEvents, DeviceEventsHandler, Keycode};
 use std::sync::mpsc;
 use std::sync::mpsc::Receiver;
 use std::thread;
 use std::time::Duration;
-use device_query::DeviceEventsHandler;
-use crossterm::event::{Event, KeyCode};
 
 pub fn listen() -> Result<Receiver<Input>, String> {
   let (sender, receiver) = mpsc::channel();
 
-  #[cfg(feature = "rdev")]
-  {
-    thread::spawn(move || rdev::listen(move |event| if let EventType::KeyPress(key) = event.event_type {
-      let input = match key {
-        Key::Num1 => Digit::One.into(),
-        Key::Num2 => Digit::Two.into(),
-        Key::Num3 => Digit::Three.into(),
-        Key::Num4 => Digit::Four.into(),
-        Key::Num5 => Digit::Five.into(),
-        Key::Num6 => Digit::Six.into(),
-        Key::Num7 => Digit::Seven.into(),
-        Key::Num8 => Digit::Eight.into(),
-        Key::Num9 => Digit::Nine.into(),
-        Key::Num0 => Digit::Zero.into(),
-        Key::Comma => Character::Decimal.into(),
-        Key::Dot => Character::Decimal.into(),
-        Key::Return => Input::Enter,
+  let device_state =
+    DeviceEventsHandler::new(Duration::from_millis(0)).unwrap_or(DeviceEventsHandler);
+
+  thread::spawn(move || {
+    let _guard = device_state.on_key_down(move |key_code| {
+      let input = match key_code {
+        Keycode::Key0 => Digit::Zero.into(),
+        Keycode::Key1 => Digit::One.into(),
+        Keycode::Key2 => Digit::Two.into(),
+        Keycode::Key3 => Digit::Three.into(),
+        Keycode::Key4 => Digit::Four.into(),
+        Keycode::Key5 => Digit::Five.into(),
+        Keycode::Key6 => Digit::Six.into(),
+        Keycode::Key7 => Digit::Seven.into(),
+        Keycode::Key8 => Digit::Eight.into(),
+        Keycode::Key9 => Digit::Nine.into(),
+        Keycode::Comma => Character::Decimal.into(),
+        Keycode::Dot => Character::Decimal.into(),
+        Keycode::Enter => Input::Enter,
         _ => return,
       };
       let _ = sender.send(input);
-    }));
-  }
-
-  #[cfg(not(feature = "rdev"))]
-  {
-    let event_state = DeviceEventsHandler::new(Duration::from_millis(10)).ok_or("failed to setup event loop")?;
-
-    thread::spawn(move || loop {
-
     });
-    
-    todo!("listen for key events")
-  }
+    loop {} //keep guard alive
+  });
 
   Ok(receiver)
 }
