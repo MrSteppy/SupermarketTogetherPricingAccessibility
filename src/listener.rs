db99bf325f736a9e600492a32ed57f3e0262cf18
Leonard Steppy <leonard.steppy@outlook.de>
2025-11-04 14:44:56 +0100

    Prepare to replace rdev with something else

--- src/listener.rs
+++ src/listener.rs
@@ -1,30 +1,39 @@
-use crate::input::{Character, Digit, Input};
-use rdev::{EventType, Key};
+use crate::input::Input;
 use std::sync::mpsc;
 use std::sync::mpsc::Receiver;
-use std::thread;
 
 pub fn listen() -> Result<Receiver<Input>, String> {
   let (sender, receiver) = mpsc::channel();
-  thread::spawn(move || rdev::listen(move |event| if let EventType::KeyPress(key) = event.event_type {
-    let input = match key {
-      Key::Num1 => Digit::One.into(),
-      Key::Num2 => Digit::Two.into(),
-      Key::Num3 => Digit::Three.into(),
-      Key::Num4 => Digit::Four.into(),
-      Key::Num5 => Digit::Five.into(),
-      Key::Num6 => Digit::Six.into(),
-      Key::Num7 => Digit::Seven.into(),
-      Key::Num8 => Digit::Eight.into(),
-      Key::Num9 => Digit::Nine.into(),
-      Key::Num0 => Digit::Zero.into(),
-      Key::Comma => Character::Decimal.into(),
-      Key::Dot => Character::Decimal.into(),
-      Key::Return => Input::Enter,
-      _ => return,
-    };
-    let _ = sender.send(input);
-  }));
+
+  #[cfg(feature = "rdev")]
+  {
+    thread::spawn(move || rdev::listen(move |event| if let EventType::KeyPress(key) = event.event_type {
+      let input = match key {
+        Key::Num1 => Digit::One.into(),
+        Key::Num2 => Digit::Two.into(),
+        Key::Num3 => Digit::Three.into(),
+        Key::Num4 => Digit::Four.into(),
+        Key::Num5 => Digit::Five.into(),
+        Key::Num6 => Digit::Six.into(),
+        Key::Num7 => Digit::Seven.into(),
+        Key::Num8 => Digit::Eight.into(),
+        Key::Num9 => Digit::Nine.into(),
+        Key::Num0 => Digit::Zero.into(),
+        Key::Comma => Character::Decimal.into(),
+        Key::Dot => Character::Decimal.into(),
+        Key::Return => Input::Enter,
+        _ => return,
+      };
+      let _ = sender.send(input);
+    }));
+  }
+
+  #[cfg(not(feature = "rdev"))]
+  {
+    crossterm::terminal::enable_raw_mode().map_err(|e| format!("{e}"))?;
+    
+    todo!("listen for key events")
+  }
 
   Ok(receiver)
 }
