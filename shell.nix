4f31330d38aafc7027290ea041090b6a5f7b3f92
Leonard Steppy <leonard.steppy@outlook.de>
2025-11-05 17:31:40 +0100

    Update shell.nix to support cross compilation for windows and remove windows.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+  mingwPkgs = pkgs.pkgsCross.mingwW64;
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     pkg-config
     xorg.libX11
     xorg.libXi
+    mingwPkgs.stdenv.cc
   ];
-  buildInputs = with pkgs; [
+
+  buildInputs = [
+    mingwPkgs.windows.mingw_w64_pthreads
   ];
 }
